b494616aba58a0e6f5179c9394c170746abb0617
frances8713 <104068962+frances8713@users.noreply.github.com>
2022-08-27 15:56:39 +0200

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom'
-import { Hello } from './Hello'
-
-const helloElement = <Hello />
-const root = document.querySelector('#root')
-
-ReactDOM.render(helloElement, root)
