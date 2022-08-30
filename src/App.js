a485db26dcc5c62b533dbb5a7e7d8e3bc204301a
Francesca Foglietta <francesfogli@gmail.com>
2022-08-30 16:10:29 +0200

    feat: exercise props-02

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { Welcome } from './Welcome'
 
 export class App extends React.Component {
     render() {
-        return < Welcome name='Polly' /> 
+        return < Welcome /> 
  
     }
 }
 
+Welcome.defaultProps = {
+    name : 'Dear'
+}
