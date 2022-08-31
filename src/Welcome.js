20f57acff411d894ce186dc0d09868cddc889796
Francesca Foglietta <francesfogli@gmail.com>
2022-08-31 13:09:42 +0200

    feat: exercise conditional-rendering-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,8 +7,10 @@ export class Welcome extends React.Component {
         return (
         <div>
             <p>Welcome, {this.props.name}!</p>
-            {(this.props.age > 18 && this.props.age < 65)  && <Age age={this.props.age}/>}
+            {(this.props.age > 18 && this.props.age < 65 && this.props.name === "John") && <Age age={this.props.age}/>}
         </div>
         )
     }
 }
+
+//Se in App lasciavo Polly si sarebbe visualizzato solo il <p> con Polly e non l'età perché l'ultima condizione non sarebbe stata avvalorata
