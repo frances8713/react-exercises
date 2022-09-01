7b08bae2e2d1ad2dbf15d9837c767c7f4d2db2da
Francesca Foglietta <francesfogli@gmail.com>
2022-09-01 11:01:34 +0200

    feat: exercise state-03

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export class Counter extends React.Component {
+    state = {
+        count : this.props.initialValue
+
+    }
+
+    constructor (props) {
+        super (props) 
+
+        setInterval (() => {
+            this.setState((state) => {
+                return {
+                    count : state.count + (this.props.incrementAmount)
+                }
+            })
+        }, this.props.incrementInterval)
+    }
+
+    render () {
+        return <h1>Count : {this.state.count}</h1>
+    }
+}
