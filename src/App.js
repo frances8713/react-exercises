9d9d6950b26b19bf534b76aa6fde54a72c9f9d75
Francesca Foglietta <francesfogli@gmail.com>
2022-09-07 23:00:57 +0200

    feat: exercise list-03

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Welcome } from './Welcome'
-import { Counter } from './Counter'
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from './Login'
-import { ClickCounter } from './ClickCounter'
-import { UncontrolledLogin } from "./UncontrolledLogin";
+import { TodoList } from './TodoList';
+
 
 
 export class App extends React.Component {
     render() {
         return (
             <div>
-                 < InteractiveWelcome />
-                 < UncontrolledLogin />
+                 < TodoList />
             </div> 
         ) 
       }
