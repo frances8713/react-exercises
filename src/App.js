fcc37f83a66531b1ffc038bfbed0100cecb7d1c8
Francesca Foglietta <francesfogli@gmail.com>
2022-09-07 09:37:01 +0200

    feat: exercise forms-01

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Welcome } from './Welcome'
 import { Counter } from './Counter'
+import { InteractiveWelcome } from "./InteractiveWelcome";
 import { ClickCounter } from './ClickCounter'
-import { ClickTracket } from "./ClickTracker";
+
 
 export class App extends React.Component {
     render() {
-        return (
-            <div>
-                < Welcome name = "John" age={35}/>
-                < ClickCounter initialValue = {10} incrementAmount = {5}/>  
-                < ClickTracket />
-            </div>
-        )
-        
-    }
+        return < InteractiveWelcome />
+         }
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -7,7 +7,6 @@ export class Welcome extends React.Component {
         return (
         <div>
             <p>Welcome, {this.props.name}!</p>
-            <Age age={this.props.age}/>
         </div>
         )
     }
