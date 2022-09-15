180fd94609e348bad80c7c44325cb3d1037d51a3
Francesca Foglietta <francesfogli@gmail.com>
2022-09-15 22:25:07 +0200

    feat: exercise useState-02

--- src/Login.js
+++ src/Login.js
@@ -1,51 +1,85 @@
-import React from "react";
 
+import { useState } from 'react'
 
-export class Login extends React.Component {
-    state = {
-        username : '',
-        password : '' ,
-        remember : false
-    }
+export function Login() {
+    const [data, setData] = useState ({
+        username: '',
+        password: '',
+        remember: false
+    })
 
-    handleInputLoginChange = (event) => {
-        const value = event.target.value
-        const name = event.target.name
-        const type = event.target.type
-        const checked = event.target.checked
+    const [disabled, setDisabled] = useState(true)
 
-        this.setState ({
-            [name]: type === 'checkbox' ? checked : value,
+    function handleInputChange (event) {
+        const {name, value, type, checked} = event.target
+
+        setData((data) => {
+            return {
+            ...data,
+            [name] : type === 'checkbox' ? checked : value
+            } 
         })
-    }
-    onLogin = () => {
-        this.props.conditions(this.state)
-    }
+    }  
 
-    handleResetButton = () => {
-        this.setState ({
-            username : '',
-            password: '',
-            remember: false
-        })
-    }
+    return ( 
+        <form>
+            <h3>Login</h3>
+            <input value={data.username} name='username' onChange={handleInputChange}/>
+            <input value={data.password} name='password' type='password' onChange={handleInputChange}/>
+            <input checked={data.remember} name='remember' type='checkbox' onChange={handleInputChange}/>  
+            <button type='submit' disabled={data.username === '' || data.password === '' ? true : false} onClick={() => setDisabled(true)}>Login</button>
+        </form>
+    )  
+} 
 
-    render () {
-        const ButtonStyle = {
-            color: this.state.password.length < 8 ? 'red' : 'green'
-        }
-        return (
-            <div>
-                <h3>Login</h3>
-                <input name = 'username' value = {this.state.username} onChange={this.handleInputLoginChange}/>
-                <input name = 'password' type ='password' value = {this.state.password} onChange={this.handleInputLoginChange}/>
-                <input name = 'remember' type='checkbox' checked={this.state.remember} onChange={this.handleInputLoginChange}/>
-                <div>
-                    <button type='submit' onClick={this.onLogin}>Login</button>
-                    <button style={ButtonStyle} onClick={this.handleResetButton}>Reset</button>
-                </div>
-            </div>
 
-        )
-    }
-}
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+
+// export class Login extends React.Component {
+//     state = {
+//         username : '',
+//         password : '' ,
+//         remember : false
+//     }
+
+//     handleInputLoginChange = (event) => {
+//         const value = event.target.value
+//         const name = event.target.name
+//         const type = event.target.type
+//         const checked = event.target.checked
+
+//         this.setState ({
+//             [name]: type === 'checkbox' ? checked : value,
+//         })
+//     }
+
+//     onLogin = () => {
+//         this.props.conditions(this.state)
+//     }
+
+//     render () {
+//         return (
+//             <div>
+//                 <h2>Form</h2>
+//                 <input name = 'username' value = {this.state.username} onChange={this.handleInputLoginChange}/>
+//                 <input name = 'password' type ='password' value = {this.state.password} onChange={this.handleInputLoginChange}/>
+//                 <input name = 'remember' type='checkbox' checked={this.state.remember} onChange={this.handleInputLoginChange}/>
+//                 <div>
+//                     <button type='submit' disabled={(this.state.username === '' || this.state.password === '') ? true : false} onClick={this.onLogin}>Login</button>
+//                 </div>
+//             </div>
+
+//         )
+//     }
+// }
