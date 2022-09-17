c401d73a501c96738b083ba84fd366c100f5114c
frances8713 <104068962+frances8713@users.noreply.github.com>
2022-09-17 15:42:05 +0200

    Update GithubUser.js

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -10,19 +10,20 @@ const [error, setError] = useState(null)
 async function fetchGithubUser(username){
     setLoad(true)
     setError(null)
-try {
-    const response = await fetch(`https://api.github.com/users/${username}`)
-    const json = await response.json()
+    
+  try {
+      const response = await fetch(`https://api.github.com/users/${username}`)
+      const json = await response.json()
 
-    setData(json)
-    console.log(json)
+      setData(json)
+      console.log(json)
 
-    } catch(error) {
-    setError(error)
-    setData(null)
-    } finally {
-   setLoad(false) 
-    }
+     } catch(error) {
+      setError(error)
+      setData(null)
+     } finally {
+      setLoad(false) 
+     }
 }
 
 useEffect (() => {
@@ -35,5 +36,5 @@ return (
             {error && <h2>There's an error</h2>}
             {data && <h2>{data.login}</h2>} 
         </div>
-)
+      )
 }
