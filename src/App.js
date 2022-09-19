3098054cc37a50e12603b6b1a8115535b32fd449
Francesca Foglietta <francesfogli@gmail.com>
2022-09-19 16:31:39 +0200

    feat: exercise useRef-02

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { Container } from './Container'
 import { GithubUser } from './GithubUser'
 import { HookCounter } from './HookCounter'
+import { CarDetails } from './CarDetalis'
 
 
 export function App () {
@@ -11,9 +12,7 @@ export function App () {
         return (
           <div> 
           <Container title="My Application">  
-          <input value={username} onChange={(e) => setUserName(e.target.value)} />      
-          <GithubUser username="frances8713"/>
-          <HookCounter />
+          <CarDetails />
           </Container>        
           </div>
         )
