257788679129708deebbc9f8b60d101041828aa6
Francesca Foglietta <francesfogli@gmail.com>
2022-09-23 01:10:59 +0200

    feat: exercise react-router-01

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { useState } from "react"
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Container } from './Container'
 import { GithubUser } from './GithubUser'
 import { HookCounter } from './HookCounter'
 import { CarDetails } from './CarDetalis'
 import { LanguageContext } from "./LanguageContext";
 import { DisplayLanguage } from "./DisplayLanguage";
-import { FilteredList } from './FilteredList'
-
+import { FilteredList } from './FilteredList';
+import { Welcome } from './Welcome';
 
 export function App () {
 
         return (
           <div>         
           <Container title="My Application">  
-          <FilteredList />
+          <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path=":name" element={<Welcome/>}/>
+          </Routes>
           </Container>             
           </div>
         )
